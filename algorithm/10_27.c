2bc26af9838bb2278a98670d2da27f985b03b2db
shixuzhimeng <1516446316@qq.com>
2025-10-27 20:12:08 +0800

    初识二分查找

--- algorithm/10_27.c
+++ algorithm/10_27.c
@@ -1,8 +1,44 @@
-int singleNumber(int* nums, int numsSize) {
-    int a = 0;
-    for(int i = 0;i < numsSize;i++)
+// int singleNumber(int* nums, int numsSize) {
+//     int a = 0;
+//     for(int i = 0;i < numsSize;i++)
+//     {
+//         a ^= nums[i];
+//     }
+//     return a;
+// }
+//二分查找
+#include <stdio.h>
+int binary_search(int arr[],int k,int sz)
+{
+    int left = 0;
+    int right = sz-1;
+    while(left <= right)
     {
-        a ^= nums[i];
+        int mid = left + (right - left)/2;
+        if(arr[mid]<k)
+            left = mid +1;
+        else if(arr[mid]>k)
+            right = mid - 1;
+        else
+        {
+            return mid;
+        }
     }
-    return a;
+    return -1;
+}
+int main()
+{
+    int arr[]= {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10};
+    int k = 7;
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    int ret = binary_search(arr,k,sz);
+    if(ret == -1)
+    {
+        printf("找不到\n");
+    }
+    else 
+    {
+        printf("找到了\n");
+    }
+    return 0;
 }
