59185ba60e155d5ff807a3ae0dbe06691309f2ba
shixuzhimeng <1516446316@qq.com>
2025-12-06 22:25:55 +0800

    practice

--- practice/12_06.c
+++ practice/12_06.c
@@ -51,68 +51,266 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// #define N 10
+
+// int dx[4] = {-1, 0, 1, 0}; 
+// int dy[4] = {0, 1, 0, -1}; 
+
+// int valid(int x, int y, char map[N][N]) {
+//     return x >= 0 && x < N && y >= 0 && y < N && map[x][y] != '*';
+// }
+
+// int main() {
+//     char map[N][N];
+//     int fx, fy, cx, cy;
+//     int fdir = 0, cdir = 0;
+
+//     for (int i = 0; i < N; i++) {
+//         scanf("%s", map[i]);
+//         for (int j = 0; j < N; j++) {
+//             if (map[i][j] == 'F') {
+//                 fx = i;
+//                 fy = j;
+//             } else if (map[i][j] == 'C') {
+//                 cx = i;
+//                 cy = j;
+//             }
+//         }
+//     }
+
+//     int time = 0;
+//     while (time < 10000)
+//     {  
+//         time++;
+//         if (fx == cx && fy == cy) {
+//             printf("%d\n", time);
+//             return 0;
+//         }
+
+//         int next_cx = cx + dx[cdir];
+//         int next_cy = cy + dy[cdir];
+//         if (valid(next_cx, next_cy, map)) {
+//             cx = next_cx;
+//             cy = next_cy;
+//         }
+//         else
+//         {
+//             cdir = (cdir + 1) % 4; 
+
+
+//         int next_fx = fx + dx[fdir];
+//         int next_fy = fy + dy[fdir];
+//         if (valid(next_fx, next_fy, map))
+//         {
+//             fx = next_fx;
+//             fy = next_fy;
+//         }
+//         else
+//         {
+//             fdir = (fdir + 1) % 4;  
+//         }
+//     }
+
+//     printf("0\n");
+//     return 0;
+// }
+
+
+// //头插法
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// struct Node {
+// 	int date ;
+// 	struct Node* next;
+// };
+
+// //插入
+// struct Node* creatList()
+// {
+// 	struct Node* headNode =(struct Node*)malloc(sizeof(struct Node));
+// 	//headNode 成了结构体变量
+// 	//变量使用必须被初始化
+// 	headNode -> date = 1;
+// 	headNode -> next = NULL;
+// 	return headNode;
+// }
+
+// //创建节点
+// struct Node* creatNode(int date)
+// {
+// 	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+// 	newNode->date = date;
+// 	newNode->next = NULL;
+// 	return newNode;
+// }
+
+// //打印链表
+// void printfList(struct Node* headNode)
+// {
+// 	struct Node* pMove = headNode->next;
+// 	while(pMove)
+// 	{
+// 		printf("%d  ", pMove->date);
+// 		pMove = pMove->next;
+// 	}
+// 	printf("\n");
+// }
+
+// //插入节点
+// void insertNode(struct Node* headNode, int date)
+// {
+// 	struct Node* newNode = creatNode(date);
+// 	newNode->next = headNode->next;
+// 	headNode->next = newNode;
+// }
+
+// //删除节点
+// void deleteNode(struct Node* headNode, int posDate)
+// {
+// 	struct Node* posNode = headNode -> next;
+// 	struct Node* posNodeFront = headNode;
+// 	if(NULL == posNode)
+// 	{
+// 		printf("无法删除  链表为空\n");
+// 	}
+// 	else
+// 	{
+// 		while(posNode->date != posDate)
+// 		{
+// 			posNodeFront = posNode;
+// 			posNode = posNodeFront->next;
+// 			if(NULL == posNode)
+// 			{
+// 				printf("没有找到 无法删除\n");
+// 				break;
+// 			}
+// 		}
+// 		posNodeFront->next = posNode->next;
+// 		free(posNode);
+// 	}
+// }
+// int main()
+// {
+// 	struct Node* list = creatList();
+// 	insertNode(list, 1);
+// 	insertNode(list, 2);
+// 	insertNode(list, 3);
+// 	printfList(list);
+// 	deleteNode(list,2);
+//     printfList(list);
+// 	system("pause");
+// 	return 0;
+// }
+
+
+//头插法
 #include <stdio.h>
+#include <stdlib.h>
 
-#define N 10
+struct student
+{
+	char name[20];
+	int num;
+	int math;
+};
 
-int dx[4] = {-1, 0, 1, 0}; 
-int dy[4] = {0, 1, 0, -1}; 
+struct Node {
+	struct student date; //数据域
+	struct Node* next;   //指针域
+};
 
-int valid(int x, int y, char map[N][N]) {
-    return x >= 0 && x < N && y >= 0 && y < N && map[x][y] != '*';
+//插入
+struct Node* creatList()
+{
+	struct Node* headNode =(struct Node*)malloc(sizeof(struct Node));
+	//headNode 成了结构体变量
+	//变量使用必须被初始化
+	//headNode -> date = 1;
+	headNode -> next = NULL;
+	return headNode;
 }
 
-int main() {
-    char map[N][N];
-    int fx, fy, cx, cy;
-    int fdir = 0, cdir = 0;
+//创建节点
+struct Node* creatNode(struct student date)
+{
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->date = date;
+	newNode->next = NULL;
+	return newNode;
+}
 
-    for (int i = 0; i < N; i++) {
-        scanf("%s", map[i]);
-        for (int j = 0; j < N; j++) {
-            if (map[i][j] == 'F') {
-                fx = i;
-                fy = j;
-            } else if (map[i][j] == 'C') {
-                cx = i;
-                cy = j;
-            }
-        }
-    }
+//打印链表
+void printfList(struct Node* headNode)
+{
+	struct Node* pMove = headNode->next;
+	printf("name\t num\t math\n");
+	while(pMove)
+	{
+		printf("%s\t %d\t %d\n", pMove->date.name,
+								 pMove->date.num,
+								 pMove->date.math);
+		pMove = pMove->next;
+	}
+	printf("\n");
+}
 
-    int time = 0;
-    while (time < 10000)
-    {  
-        time++;
-        if (fx == cx && fy == cy) {
-            printf("%d\n", time);
-            return 0;
-        }
+//插入节点
+void insertNode(struct Node* headNode, struct student date)
+{
+	struct Node* newNode = creatNode(date);
+	newNode->next = headNode->next;
+	headNode->next = newNode;
+}
 
-        int next_cx = cx + dx[cdir];
-        int next_cy = cy + dy[cdir];
-        if (valid(next_cx, next_cy, map)) {
-            cx = next_cx;
-            cy = next_cy;
-        }
-        else
-        {
-            cdir = (cdir + 1) % 4; 
-
-
-        int next_fx = fx + dx[fdir];
-        int next_fy = fy + dy[fdir];
-        if (valid(next_fx, next_fy, map))
-        {
-            fx = next_fx;
-            fy = next_fy;
-        }
-        else
-        {
-            fdir = (fdir + 1) % 4;  
-        }
-    }
-
-    printf("0\n");
-    return 0;
+//删除节点
+void deleteNode(struct Node* headNode, int num)
+{
+	struct Node* posNode = headNode -> next;
+	struct Node* posNodeFront = headNode;
+	if(NULL == posNode)
+	{
+		printf("无法删除  链表为空\n");
+	}
+	else
+	{
+		while(posNode->date.num != num)
+		{
+			posNodeFront = posNode;
+			posNode = posNodeFront->next;
+			if(NULL == posNode)
+			{
+				printf("没有找到 无法删除\n");
+				break;
+			}
+		}
+		posNodeFront->next = posNode->next;
+		free(posNode);
+	}
+}
+int main()
+{
+	struct Node* list = creatList();
+	struct student info;
+	while(1)
+	{
+		scanf("%s %d %d",info.name,&info.num, &info.math);
+		insertNode(list,info);
+		printf("continue(Y/N)?\n");
+		char choice = 0;
+		setbuf(stdin, NULL);
+		scanf("%c",&choice);
+		if(choice == 'N' || choice == 'n')
+		{
+			break;
+		}
+	}
+	printfList(list);
+	scanf("%d",&info.num);
+	deleteNode(list,info.num);
+	printfList(list);
+	system("pause");
+	return 0;
 }
