ab62dd9c2c32abfc0ad40ef343e946a58661ca4b
shixuzhimeng <1516446316@qq.com>
2025-12-08 22:29:28 +0800

    practice

--- practice/11_25.c
+++ practice/11_25.c
@@ -43,3 +43,48 @@
 //     fclose(fp);
 //     return 0;
 // }
+
+
+// #include <dirent.h>
+// #include "tlpi_hdr.h"
+
+// static void     /* List all files in directory 'dirPath' */
+// listFiles(const char *dirpath)
+// {
+// 	DIR *drip;
+// 	struct dirent *dp;
+// 	Boolean isCurrent;   /* Ture if 'dirpath' is "." */
+	
+// 	isCurrent = strcmp(dirpath, ".") == 0;
+// 	dirp = opendir(dirpath);
+// 	if(dirp == NULL)
+// 	{
+// 		errMsg("opendir failed on '%s'", dirpath);
+// 		return ;
+// 	}
+	
+// 	/* For each entry in this directory, print directory + filename */
+	
+// 	for(;;)
+// 	{
+// 	    errno == 0;
+// 	    dp = readdir(dirp);
+// 	    if(dirp == NULL)
+// 	    {
+// 		    break;          /* To distinguish error from end-of-directory */
+// 	    }
+// 	    if(strcmp(dp -> d_name, ".") == 0 || strcmp(dp -> d_name, "..") == 0)
+// 	    {
+// 		    continue;        /* Skip . and .. */
+// 	    }
+// 	    if(!isCurrent)
+// 	    {
+// 		    printf("%s/",dirpath);
+// 		}
+// 		printf("%s\n",dp -> d_name);
+// 	}
+	
+// 	if(errno != 0)
+// 	errExit("readdir");
+	
+// }
