875bd95a7e56ce3fce36b5de92788ec865bb09d0
shixuzhimeng <1516446316@qq.com>
2025-12-22 17:24:01 +0800

    practice

---
+++ practice/12_22.c
@@ -0,0 +1,41 @@
+// #include <stdio.h>
+
+// void F1()
+// {
+//     int b = 0;
+//     printf("%p\n",&b);
+// }
+
+// void F2()
+// {
+//     int a = 0;
+//     printf("%p\n",&a);
+// }
+
+// int main()
+// {
+//     F1();
+//     F2();
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// void Add(int x , int y)
+// {
+//     int z = 0;
+//     z = x + y;
+//     return z;
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     int b = 10;
+//     int c = 0;
+
+//     c = Add(a ,b);
+
+//     printf("%d\n",c)
+// }
